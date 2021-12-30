67d358719957369f1b75b4e74299fab075031e4d
Travis Hinton <t@travishinton.us>
2021-12-29 19:46:59 -0700

    Fixed image position

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <section class="home">
-    <div class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+    <div class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-start">
       <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
         <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
       </div>
-      <div class="flex flex-col w-full xl:w-2/5 items-start">
+      <div class="flex flex-col w-full xl:w-2/5">
         <img
           alt="Hero"
           class="rounded shadow-xl"
